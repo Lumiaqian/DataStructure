af718ebe14d09724ab0478d6a48038b062c852f0
caojingjing <lumia.cao@blackshark.com>
2022-11-01 14:21:17 +0800

    :alembic: add doubly_circular_linked_list

--- list/doublycircularlinkedlist/doublyCircularLinkedList.go
+++ list/doublycircularlinkedlist/doublyCircularLinkedList.go
@@ -1,65 +1,65 @@
-package doublelinklist
+package doublycircularlinkedlist
 
-type DoubleNode[T any] struct {
+type Node[T any] struct {
 	Value T
-	Prev  *DoubleNode[T]
-	Next  *DoubleNode[T]
+	Prev  *Node[T]
+	Next  *Node[T]
 }
 
-type DoubleLinkList[T any] struct {
-	head *DoubleNode[T]
-	tail *DoubleNode[T]
+type List[T any] struct {
+	head *Node[T]
+	tail *Node[T]
 	size int
 }
 
-// 初始化双向链表
-func (l *DoubleLinkList[T]) Init() *DoubleLinkList[T] {
-	l.head = new(DoubleNode[T])
-	l.tail = new(DoubleNode[T])
+// 初始化Doubly Circular Linked List detail
+func (l *List[T]) Init() *List[T] {
+	l.head = new(Node[T])
+	l.tail = new(Node[T])
 	l.head.Next = l.tail
-	l.head.Prev = nil
 	l.tail.Prev = l.head
-	l.tail.Next = nil
+	l.head.Prev = l.tail
+	l.tail.Next = l.head
 	l.size = 0
 	return l
 }
 
-// 新建双向链表
-func (l *DoubleLinkList[T]) New() *DoubleLinkList[T] {
-	return new(DoubleLinkList[T]).Init()
+// 初始化Doubly Circular Linked List
+func (l *List[T]) New() *List[T] {
+	return new(List[T]).Init()
 }
 
 // 获取链表长度
-func (l *DoubleLinkList[T]) Len() int {
+func (l *List[T]) Len() int {
 	return l.size
 }
 
 // 判断链表是否为空
-func (l *DoubleLinkList[T]) IsEmpty() bool {
+func (l *List[T]) IsEmpty() bool {
 	if l.size == 0 {
 		return true
 	}
 	return false
 }
 
-// 获取链表头元素
-func (l *DoubleLinkList[T]) Front() *DoubleNode[T] {
-	if l.size == 0 {
+// 获取头节点
+func (l *List[T]) Front() *Node[T] {
+	if l.IsEmpty() {
 		return nil
 	}
 	return l.head.Next
 }
 
-// 获取链表尾元素
-func (l *DoubleLinkList[T]) Back() *DoubleNode[T] {
-	if l.size == 0 {
+// 获取尾节点
+func (l *List[T]) Back() *Node[T] {
+	if l.IsEmpty() {
 		return nil
 	}
 	return l.tail.Prev
 }
 
 // 获取指定索引上的元素
-func (l *DoubleLinkList[T]) Get(index int) *DoubleNode[T] {
+func (l *List[T]) Get(index int) *Node[T] {
 	if index <= 0 || index > l.size {
 		return nil
 	}
@@ -76,23 +76,24 @@ func (l *DoubleLinkList[T]) Get(index int) *DoubleNode[T] {
 	return cur
 }
 
-// 指定向指定位置插入元素
-func (l *DoubleLinkList[T]) Insert(index int, value T) {
+// 插入指定位置
+func (l *List[T]) Insert(index int, value T) {
 	if index <= 0 || index > l.size+1 {
 		return
 	}
-	node := &DoubleNode[T]{Value: value}
+	node := &Node[T]{Value: value}
 	if index == 1 {
 		node.Next = l.head.Next
+		node.Prev = l.head
 		l.head.Next.Prev = node
 		l.head.Next = node
-		node.Prev = l.head
 		l.size++
 		return
 	}
 	if index == l.size+1 {
-		l.tail.Prev.Next = node
-		node.Prev = l.tail.Prev
+		pre := l.tail.Prev
+		pre.Next = node
+		node.Prev = pre
 		node.Next = l.tail
 		l.tail.Prev = node
 		l.size++
@@ -103,16 +104,16 @@ func (l *DoubleLinkList[T]) Insert(index int, value T) {
 		cur = cur.Next
 	}
 	next := cur.Next
-	node.Next = next
 	next.Prev = node
+	node.Next = next
 	cur.Next = node
 	node.Prev = cur
 	l.size++
 }
 
 // 从头部插入元素
-func (l *DoubleLinkList[T]) PushFront(value T) {
-	node := &DoubleNode[T]{Value: value}
+func (l *List[T]) PushFront(value T) {
+	node := &Node[T]{Value: value}
 	node.Next = l.head.Next
 	node.Prev = l.head
 	l.head.Next.Prev = node
@@ -121,8 +122,8 @@ func (l *DoubleLinkList[T]) PushFront(value T) {
 }
 
 // 从尾部插入元素
-func (l *DoubleLinkList[T]) PushBack(value T) {
-	node := &DoubleNode[T]{Value: value}
+func (l *List[T]) PushBack(value T) {
+	node := &Node[T]{Value: value}
 	l.tail.Prev.Next = node
 	node.Prev = l.tail.Prev
 	node.Next = l.tail
@@ -131,21 +132,21 @@ func (l *DoubleLinkList[T]) PushBack(value T) {
 }
 
 // LPush,类似redis lpush命令，往头位置插入
-func (l *DoubleLinkList[T]) LPush(values ...T) {
+func (l *List[T]) LPush(values ...T) {
 	for _, v := range values {
 		l.PushFront(v)
 	}
 }
 
 // RPush,类似redis rpush命令,往尾部位置插入
-func (l *DoubleLinkList[T]) RPush(values ...T) {
+func (l *List[T]) RPush(values ...T) {
 	for _, v := range values {
 		l.PushBack(v)
 	}
 }
 
 // 删除指定索引上的元素
-func (l *DoubleLinkList[T]) Del(index int) {
+func (l *List[T]) Del(index int) {
 	if index <= 0 || index > l.size {
 		return
 	}
@@ -159,7 +160,7 @@ func (l *DoubleLinkList[T]) Del(index int) {
 }
 
 // 删除指定元素
-func (l *DoubleLinkList[T]) Remove(fn func(value T) bool) {
+func (l *List[T]) Remove(fn func(value T) bool) {
 	if l.size == 0 {
 		return
 	}
@@ -176,7 +177,7 @@ func (l *DoubleLinkList[T]) Remove(fn func(value T) bool) {
 }
 
 // 按值查找，判断是否包含这个value
-func (l *DoubleLinkList[T]) Locate(fn func(value T) bool) bool {
+func (l *List[T]) Locate(fn func(value T) bool) bool {
 	if l.size == 0 {
 		return false
 	}
@@ -191,7 +192,7 @@ func (l *DoubleLinkList[T]) Locate(fn func(value T) bool) bool {
 }
 
 // list转slice
-func (l *DoubleLinkList[T]) ToSlice() []T {
+func (l *List[T]) ToSlice() []T {
 	if l.size == 0 {
 		return nil
 	}
---
+++ list/doublycircularlinkedlist/doublyCircularLinkedList_test.go
@@ -0,0 +1,230 @@
+package doublycircularlinkedlist
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestLPush(t *testing.T) {
+	Convey("Test LPush", t, func() {
+		var listA List[int]
+		var listB List[string]
+		listA = *listA.New()
+		listB = *listB.New()
+		listA.LPush(1, 2, 3, 4, 5)
+		listB.LPush("a", "b", "c", "d", "e")
+		Convey("The result of LPush:5,4,3,2,1", func() {
+			So(listA.ToSlice(), ShouldResemble, []int{5, 4, 3, 2, 1})
+		})
+		Convey("The result of LPush:e,d,c,b,a", func() {
+			So(listB.ToSlice(), ShouldResemble, []string{"e", "d", "c", "b", "a"})
+		})
+	})
+}
+
+func TestRPush(t *testing.T) {
+	Convey("Test RPush", t, func() {
+		var listA List[int]
+		var listB List[string]
+		listA = *listA.New()
+		listB = *listB.New()
+		listA.RPush(1, 2, 3, 4, 5)
+		listB.RPush("a", "b", "c", "d", "e")
+		Convey("The result of RPush:1,2,3,4,5", func() {
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+		})
+		Convey("The result of RPush:a,b,c,d,e", func() {
+			So(listB.ToSlice(), ShouldResemble, []string{"a", "b", "c", "d", "e"})
+		})
+	})
+}
+
+func TestIsEmpty(t *testing.T) {
+	Convey("Test IsEmpty", t, func() {
+		var listA List[int]
+		var listB List[string]
+		listA = *listA.New()
+		listB = *listB.New()
+		listA.LPush(1, 2, 3, 4, 5)
+		Convey("The result of IsEmpty:false", func() {
+			So(listA.IsEmpty(), ShouldBeFalse)
+		})
+		Convey("The result of IsEmpty:true", func() {
+			So(listB.IsEmpty(), ShouldBeTrue)
+		})
+	})
+}
+
+func TestLen(t *testing.T) {
+	Convey("Test Len", t, func() {
+		var listA List[int]
+		var listB List[string]
+		listA = *listA.New()
+		listB = *listB.New()
+		listA.LPush(1, 2, 3, 4, 5)
+		Convey("The result of Len:5", func() {
+			So(listA.Len(), ShouldEqual, 5)
+		})
+		Convey("The result of Len:0", func() {
+			So(listB.Len(), ShouldEqual, 0)
+		})
+	})
+}
+
+func TestFront(t *testing.T) {
+	Convey("Test Front", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		Convey("The result of Front:nil", func() {
+			So(listA.Front(), ShouldBeNil)
+		})
+		Convey("The result of Front:10", func() {
+			listA.PushFront(10)
+			So(listA.Front().Value, ShouldEqual, 10)
+		})
+	})
+}
+func TestBack(t *testing.T) {
+	Convey("Test Back", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		Convey("The result of Back:nil", func() {
+			So(listA.Back(), ShouldBeNil)
+		})
+		Convey("The result of Back:10", func() {
+			listA.LPush(1, 2, 3, 4, 5)
+			listA.PushBack(10)
+			So(listA.Back().Value, ShouldEqual, 10)
+		})
+	})
+}
+
+func TestGet(t *testing.T) {
+	Convey("Test Get", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		Convey("The result of Get:nil", func() {
+			So(listA.Get(0), ShouldBeNil)
+			So(listA.Get(1), ShouldBeNil)
+		})
+		Convey("The result of Get:10,30,50", func() {
+			listA.RPush(10, 20, 30, 40, 50)
+			So(listA.Get(1).Value, ShouldEqual, 10)
+			So(listA.Get(3).Value, ShouldEqual, 30)
+			So(listA.Get(5).Value, ShouldEqual, 50)
+		})
+	})
+}
+
+func TestInsert(t *testing.T) {
+	Convey("Test Insert", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		listA.RPush(1, 2, 3, 4, 5)
+		Convey("The result of Insert:1,2,3,4,5,6", func() {
+			listA.Insert(6, 6)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5, 6})
+		})
+		Convey("The result of Insert:6,1,2,3,4,5", func() {
+			listA.Insert(1, 6)
+			So(listA.ToSlice(), ShouldResemble, []int{6, 1, 2, 3, 4, 5})
+		})
+		Convey("The result of Insert:1,2,6,3,4,5", func() {
+			listA.Insert(3, 6)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 6, 3, 4, 5})
+		})
+		Convey("The result of Insert:1,2,3,4,5", func() {
+			listA.Insert(0, 6)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+			listA.Insert(7, 6)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+		})
+	})
+}
+
+func TestDel(t *testing.T) {
+	Convey("Test Del", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		listA.RPush(1, 2, 3, 4, 5)
+		Convey("The result of Del:1,2,3,4,5", func() {
+			listA.Del(0)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 5)
+			listA.Del(6)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 5)
+		})
+		Convey("The result of Del:2,3,4,5", func() {
+			listA.Del(1)
+			So(listA.ToSlice(), ShouldResemble, []int{2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 4)
+		})
+		Convey("The result of Del:1,2,4,5", func() {
+			listA.Del(3)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 4, 5})
+			So(listA.size, ShouldEqual, 4)
+		})
+		Convey("The result of Del:1,2,3,4", func() {
+			listA.Del(5)
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4})
+			So(listA.size, ShouldEqual, 4)
+		})
+	})
+}
+
+func TestRemove(t *testing.T) {
+	Convey("Test Remove", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		Convey("The result of Remove:empty", func() {
+			listA.Remove(func(value int) bool { return 1 == value })
+			So(listA.ToSlice(), ShouldBeEmpty)
+			So(listA.size, ShouldEqual, 0)
+		})
+		Convey("The result of Remove:1,2,3,4,5", func() {
+			listA.RPush(1, 2, 3, 4, 5)
+			listA.Remove(func(value int) bool { return 0 == value })
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 5)
+			listA.Remove(func(value int) bool { return 6 == value })
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 5)
+		})
+		Convey("The result of Remove:2,3,4,5", func() {
+			listA.RPush(1, 2, 3, 4, 5)
+			listA.Remove(func(value int) bool { return 1 == value })
+			So(listA.ToSlice(), ShouldResemble, []int{2, 3, 4, 5})
+			So(listA.size, ShouldEqual, 4)
+		})
+		Convey("The result of Remove:1,2,4,5", func() {
+			listA.RPush(1, 2, 3, 4, 5)
+			listA.Remove(func(value int) bool { return 3 == value })
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 4, 5})
+			So(listA.size, ShouldEqual, 4)
+		})
+		Convey("The result of Remove:1,2,3,4", func() {
+			listA.RPush(1, 2, 3, 4, 5)
+			listA.Remove(func(value int) bool { return 5 == value })
+			So(listA.ToSlice(), ShouldResemble, []int{1, 2, 3, 4})
+			So(listA.size, ShouldEqual, 4)
+		})
+	})
+}
+
+func TestLocate(t *testing.T) {
+	Convey("Test Locate", t, func() {
+		var listA List[int]
+		listA = *listA.New()
+		Convey("The result of Locate:false", func() {
+			So(listA.Locate(func(value int) bool { return 1 == value }), ShouldBeFalse)
+			listA.RPush(1, 2, 3, 4, 5)
+			So(listA.Locate(func(value int) bool { return 0 == value }), ShouldBeFalse)
+		})
+		Convey("The result of Locate:true", func() {
+			listA.RPush(10, 20, 30, 40, 50)
+			So(listA.Locate(func(value int) bool { return 30 == value }), ShouldBeTrue)
+		})
+	})
+}
